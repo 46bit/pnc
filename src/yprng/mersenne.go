a4751f168d59d0816ea22a94f5e75a22b692e0c2
Michael Mokrysz <hi@46bit.com>
2013-12-21 13:17:49 +0000

    Added tausworthe generator (refined version from beating-the-slot-machines), restructured to use a consistent API.

--- src/yprng/mersenne.go
+++ src/yprng/mersenne.go
@@ -1,18 +1,7 @@
 package main
 
 import (
-//  "os"
   "fmt"
-//  "log"
-//  "time"
-//  "math/rand"
-//  "bytes"
-//  "bufio"
-//  "io/ioutil"
-//  "regexp"
-//  "strings"
-//  "strconv"
-//  "encoding/json"
 )
 
 // with the restriction that 2nw − r − 1 is a Mersenne prime
@@ -30,42 +19,41 @@ const (
   l = iota // additional Mersenne Twister tempering bit shifts
 )
 
-type Mt struct {
-  state [624]int
+type MersenneTwister struct {
+  State [624]uint32
   index int
 }
 
-func NewMt(seed int) Mt {
-  m := Mt{}
-  m.index = 0
-  m.initialize_generator(seed)
+func NewMersenneTwister() MersenneTwister {
+  m := MersenneTwister{}
   return m
 }
 
-func (m *Mt) initialize_generator(seed int) {
-  m.state[0] = seed
+func (m *MersenneTwister) Seed(seed uint32) {
+  m.index = 0
+  m.State[0] = seed
   for i := 1; i < 624; i++ {
-    t := m.state[i - 1] ^ (m.state[i - 1] >> 30)
-    m.state[i] = 1812433253 * (t + i)
+    t := m.State[i - 1] ^ (m.State[i - 1] >> 30)
+    m.State[i] = 1812433253 * (t + uint32(i))
   }
 }
 
-func (m *Mt) generate_numbers() {
+func (m *MersenneTwister) generate_numbers() {
   for i := 0; i < 624; i++ {
-    y := (m.state[i] & 0x80000000) + (m.state[(i + 1) % 624] & 0x7fffffff)
-    m.state[i] = m.state[(i + 397) % 624] ^ (y >> 1)
+    y := (m.State[i] & 0x80000000) + (m.State[(i + 1) % 624] & 0x7fffffff)
+    m.State[i] = m.State[(i + 397) % 624] ^ (y >> 1)
     if y % 2 != 0 {
-      m.state[i] = m.state[i] ^ 2567483615
+      m.State[i] = m.State[i] ^ 2567483615
     }
   }
 }
 
-func (m *Mt) extract_number() int {
+func (m *MersenneTwister) Urand32() uint32 {
   if m.index == 0 {
     m.generate_numbers()
   }
 
-  y := m.state[m.index]
+  y := m.State[m.index]
   y = y ^ (y >> 11)
   y = y ^ ((y << 7) & 2636928640)
   y = y ^ ((y << 15) & 4022730752)
@@ -78,11 +66,11 @@ func (m *Mt) extract_number() int {
 func main() {
   debug := true
 
-  m := NewMt(0)
-  //for i := 0; i < 1000000; i++ {
-  //r := 0
-  for r, i := 0, 0; r != 1; i++ {
-    r = m.extract_number()
+  m := NewMersenneTwister()
+  m.Seed(0)
+
+  for i := 0; r < 1000000; i++ {
+    r := m.Urand32()
     fmt.Printf("%dth value is %d\n", i, r)
   }
 
---
+++ src/yprng/tausworthe.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+//  "os"
+  "fmt"
+  "io/ioutil"
+  "encoding/json"
+)
+
+const (
+  p = 98
+  L = 32
+  d = 100 * p
+  r = 5000 * p
+  lfsr_width = d * L
+)
+
+type TauswortheGenerator struct {
+  Registers [lfsr_width]uint32
+}
+
+func NewTauswortheGenerator() TauswortheGenerator {
+  t := TauswortheGenerator{}
+  return t
+}
+
+func (t *TauswortheGenerator) Seed(seed uint32) {
+  fmt.Println("TauswortheGenerator.Seed: note that seeding this takes ages due to slow mixing")
+  for pi := 0; pi < lfsr_width; pi++ {
+    if (seed >> (uint32(pi) % 32)) % 2 == 1 {
+      t.Registers[pi] = 1
+    } else {
+      t.Registers[pi] = 0
+    }
+  }
+  for ri := 0; ri < r; ri++ {
+    t.Urand32()
+  }
+}
+
+func (t *TauswortheGenerator) generate_number() {
+  v := (t.Registers[lfsr_width - 33] + t.Registers[lfsr_width - 48]) % 2
+  for lfsri := 1; lfsri < lfsr_width; lfsri++ {
+    t.Registers[lfsri - 1] = t.Registers[lfsri]
+  }
+  t.Registers[lfsr_width - 1] = v
+}
+
+func (t *TauswortheGenerator) Urand32() uint32 {
+  t.generate_number()
+  v := uint32(0)
+  for Li := 0; Li < L; Li++ {
+    v = v * 2 + uint32(t.Registers[Li * d])
+  }
+  return v
+}
+
+func (t *TauswortheGenerator) AsJSON() ([]byte, error) {
+  t_json, err := json.Marshal(t)
+  if err != nil {
+    return []byte{}, err
+  }
+  return t_json, nil
+}
+
+func NewTauswortheGeneratorFromJSON(tg_json []byte) (TauswortheGenerator, error) {
+  var t TauswortheGenerator
+  err := json.Unmarshal(tg_json, &t)
+  if err != nil {
+    return t, err
+  }
+  return t, nil
+}
+
+func NewTauswortheGeneratorFromJSONFile(jpath string) (TauswortheGenerator, error) {
+  tausworthe_generator_json, err := ioutil.ReadFile(jpath)
+  if err != nil {
+    return TauswortheGenerator{}, err
+  }
+  return NewTauswortheGeneratorFromJSON(tausworthe_generator_json)
+}
+
+func main() {
+  debug := true
+
+  t := NewTauswortheGenerator()
+  t.Seed(987234789)
+
+  /*
+  // Print the generator post-seed as JSON.
+  t_json, _ := t.AsJSON()
+  fmt.Println(string(t_json))
+  */
+
+  // Load the generator from a JSON file. Handy for getting values quickly with a
+  // known seed, given how long the mixing takes...
+  // t, _ := NewTauswortheGeneratorFromJSONFile(os.Args[1])
+
+  for wi := 0; wi < 1000000; wi++ {
+    // @TODO FIX: debug bias towards repeat values. Generating intermediate values is a
+    // BAD temporary workaround.
+    t.Urand32()
+    w_value := t.Urand32()
+
+    fmt.Printf("%dth value is %d\n", wi, w_value)
+  }
+
+  if debug { fmt.Println("EOF") }
+}
